ef9fbbf4cd49bf5eaa1675d8174f1ee69c65b7c4
Richard Dimalanta <richard.dimalanta@gmail.com>
2015-04-02 11:44:24 +0800

    Added sample data for token test.

--- src/main/resources/db/db_script.sql
+++ src/main/resources/db/db_script.sql
@@ -1,5 +1,7 @@
 -- oauth_client_details
 
+DROP TABLE IF EXISTS oauth_client_details;
+
 CREATE TABLE oauth_client_details (
   client_id varchar(256) NOT NULL,
   resource_ids varchar(256) DEFAULT NULL,
@@ -15,7 +17,13 @@ CREATE TABLE oauth_client_details (
   PRIMARY KEY (client_id)
 );
 
+INSERT INTO oauth_client_details(client_id, resource_ids, client_secret, scope, authorized_grant_types, authorities, access_token_validity, refresh_token_validity)
+VALUES ('admin@test.com', 'rest_app', '$2a$10$/EyYRhXA4d4wjTbO4BaxceuR98MfeWbl9E7oolyVYVhgIC1dTzAlO', 'trust,read,write', 
+			'client_credentials,authorization_code,implicit,password,refresh_token', 'ROLE_USER', '0', '0');
+
 -- oauth_access_token
+
+DROP TABLE IF EXISTS oauth_access_token;
 
 CREATE TABLE oauth_access_token (
   token_id varchar(256) DEFAULT NULL,
@@ -28,6 +36,8 @@ CREATE TABLE oauth_access_token (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- aouth_refresh_token
+
+DROP TABLE IF EXISTS oauth_refresh_token;
 
 CREATE TABLE oauth_refresh_token (
   token_id varchar(256) DEFAULT NULL,
